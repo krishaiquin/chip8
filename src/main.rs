1474ab60992c21a4fcec01b60a507ab3af6159ef
krisha <krishaiquin@gmail.com>
2026-05-05 15:31:47 -0700

    Add logical and arithmetic instructions, except shift

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ const NUM_REG: usize = 16;
 const SCREEN_HEIGHT: usize = 32;
 const SCREEN_WIDTH: usize = 64;
 //const FONT_SIZE: usize = 80;
-//const STACK_SIZE: usize = 16;
+const STACK_SIZE: usize = 16; 
 const BYTE: usize = 8;
 const FLAG_REGISTER: usize = 0xF;
 const INSTRUCTIONS_PER_SECOND: u64 = 700;
@@ -45,8 +45,8 @@ struct Chip8 {
     memory: [u8; RAM_SIZE],
     i_reg: u16, //holds the memory addr of sprite data
     pc: u16,
-    //stack: [u16; STACK_SIZE],
-    registers: [u8; NUM_REG],
+    stack: Vec<u16>,
+    registers: [i8; NUM_REG],
     //delay_timer: u8,
     //sound_timer: u8,
     display: [bool; SCREEN_HEIGHT * SCREEN_WIDTH],
@@ -68,6 +68,13 @@ impl Chip8 {
         }
         
     }
+
+    fn combine_u8(&self, hi: u8, lo: u8) -> i8 {
+        ((hi << 4) | lo) as i8
+    }
+    fn combine_u16(&self, hi: u8, mid: u8, lo: u8) -> u16 {
+        (hi << 8) as u16 | (mid << 4) as u16 | lo as u16
+    } 
     fn fetch(&mut self) -> u16 {
         let instr_hi = self.memory[self.pc as usize];
         let instr_lo = self.memory[self.pc as usize + 1];
@@ -81,73 +88,201 @@ impl Chip8 {
 
     //decode happens here too
     fn execute(&mut self, instr: u16) {
-        let nibble: u8 = ((instr & 0xFF00) >> 12) as u8;
-        debug_println!("opcode: {:02X?}", nibble);
+        let nibble1: u8 = ((instr & 0xF000) >> 12) as u8;
+        let nibble2: u8 = ((instr & 0xF00) >> 8) as u8;
+        let nibble3: u8 = ((instr & 0xF0) >> 4) as u8;
+        let nibble4: u8 = (instr & 0xF)  as u8;
+        debug_println!("instr: {:04X?}", instr);
+        debug_println!("nibbles: {:02X?}, {:02X?}, {:02X?}, {:02X?}", nibble1, nibble2, nibble3, nibble4);
 
-        match nibble {
-           0x0 =>  {
-            self.display = [false; SCREEN_HEIGHT * SCREEN_WIDTH]
-           },
-           0x1 => {
-            let addr = instr & 0xFFF;
-            self.pc = addr;
-            debug_println!("pc = {:04X?}", self.pc);
-            debug_println!("addr = {:04X?}", addr);
-           },
-           0x6 => {
-            let reg = ((instr & 0xF00) >> BYTE) as usize;
-            let val = (instr & 0xFF) as u8;
-            self.registers[reg] = val;
-            debug_println!("Register[{reg}]: {:02X?}", self.registers[reg]);
-           },
-           0x7 => {
-            let reg = ((instr & 0xF00) >> BYTE) as usize;
-            let val = (instr & 0xFF) as u8;
-            self.registers[reg] += val;
-           }
-           0xA => {
-            let addr = instr & 0xFFF;
-            self.i_reg = addr;
-            debug_println!("i_reg: {:04X?}", self.i_reg);
-           }
-           0xD => {
-            let reg_x = ((instr & 0xF00) >> BYTE) as usize;
-            let reg_y = ((instr & 0x0F0) >> 4) as usize;
-            let x = self.registers[reg_x] as usize % SCREEN_WIDTH;
-            let y = self.registers[reg_y] as usize % SCREEN_HEIGHT;
-            let height = (instr & 0xF) as usize;
-            
-            self.registers[FLAG_REGISTER] = 0;
-
-            debug_println!("register[{reg_x}]: {x}");
-            debug_println!("register[{reg_y}]: {y}");
-            debug_println!("height(hex,dec): ({:02X?}, {height})", {height});
-            //set display
-            for i in 0..height {
-                if y + i <= SCREEN_HEIGHT {
-                    let sprite_data = self.memory[(self.i_reg as usize) + i];
-                    //for each bit in sprite_data
-                    for bit in 0..BYTE {
-                        if y + bit <= SCREEN_WIDTH {
-                            let sprite_bit = (sprite_data >> ((BYTE - 1) - bit)) & 1;
-                            let flatten_coord = ((y + i) * SCREEN_WIDTH) + (x + bit);
-                            let screen_pixel = &mut self.display[flatten_coord];
-
-                            if sprite_bit == 1 && *screen_pixel == true {
-                                self.registers[FLAG_REGISTER] = 1;
-                                *screen_pixel = false;
-                            } else if sprite_bit == 1 && *screen_pixel == false {
-                                *screen_pixel = true;
-                            }
-                        }   
-                    }
+        match (nibble1, nibble2, nibble3, nibble4) {
+            (0x0, 0x0, 0xE, 0x0) => {
+                self.display = [false; SCREEN_HEIGHT * SCREEN_WIDTH];
+                self.render();
+            },
+            (0x1, _, _, _) => {
+                let addr = self.combine_u16(nibble2, nibble3, nibble4);
+                self.pc = addr;
+                debug_println!("(pc = addr): ({:04X?} = {:04X?})", self.pc, addr);
+            },
+            (0x2, _, _, _) => {
+                let addr = self.combine_u16(nibble2, nibble3, nibble4);
+                self.stack.push(self.pc);
+                debug_println!("PC {:04X?} pushed to stack!", self.stack.last().expect("Expecting a value from stack but got None."));
+                self.pc = addr;
+                debug_println!("PC has been set to {:04X?}", self.pc);
+            },
+            (0x0, 0x0, 0xE, 0xE) => {
+                debug_println!("Popping {:04X?} from stack.", self.stack.last());
+                self.pc = self.stack.pop().expect("Expecting a value from stack but got None.");
+                debug_println!("PC has been set to {:04X?}", self.pc);
+            },
+            (0x3, _, _, _) => {
+                let regx = self.registers[nibble2 as usize];
+                let value = self.combine_u8(nibble3, nibble4);
+                debug_println!("(regx == value): ({regx} == {value})"); 
+                if regx == value {
+                    self.pc += 2;
                 }
-            }
+            },
+            (0x4, _, _, _) => {
+                let regx = self.registers[nibble2 as usize];
+                let value = self.combine_u8(nibble3, nibble4);
+                debug_println!("(regx != value): ({regx} != {value})"); 
+                if regx != value {
+                    self.pc += 2;
+                }
+            },
+            (0x5, _, _, 0) => {
+                let regx = self.registers[nibble2 as usize];
+                let regy = self.registers[nibble3 as usize];
+                debug_println!("(regx == regy): ({regx} == {regy})"); 
+                if regx == regy {
+                    self.pc += 2;
+                }
+            },
+            (0x9, _, _, 0) => {
+                let regx = self.registers[nibble2 as usize];
+                let regy = self.registers[nibble3 as usize];
+                debug_println!("(regx != regy): ({regx} != {regy})"); 
+                if regx != regy {
+                    self.pc += 2;
+                }
+            },
+            (0x6, _, _, _) => {
+                let value = self.combine_u8(nibble3, nibble4);
+                self.registers[nibble2 as usize] = value;
+            }, 
+            (0x7, _, _, _) => {
+                let value = self.combine_u8(nibble3, nibble4); 
+                self.registers[nibble2 as usize] += value;
+ 
+            },
+            (0x8, _, _, 0) => { 
+                self.registers[nibble2 as usize] = self.registers[nibble3 as usize];
+            },
+            (0x8, _, _, 1) => {
+                self.registers[nibble2 as usize] = self.registers[nibble2 as usize] | self.registers[nibble3 as usize];
+            },
+            (0x8, _, _, 2) => {
+                self.registers[nibble2 as usize] = self.registers[nibble2 as usize] & self.registers[nibble3 as usize];
+            },
+            (0x8, _, _, 3) => {
+                self.registers[nibble2 as usize] = self.registers[nibble2 as usize] ^ self.registers[nibble3 as usize];
+            },
+            //
+            // Overflow occurs when :
+            // 1. Two negative numbers are added and an answer comes positive
+            // 2. Two positive numbers are added and an answer comes negative
+            //
+            (0x8, _, _, 4) => {
+                let regx = self.registers[nibble2 as usize];
+                let regy = self.registers[nibble3 as usize]; 
+                let result = regx + regy;
+                self.registers[nibble2 as usize] = result;
 
-           }
-           _ => {
-            println!("Not implemented yet!");
-           }
+                if (regx < 0 && regy < 0 && result > 0) || (regx > 0 && regy > 0 && result < 0) {
+                    self.registers[FLAG_REGISTER] = 1;
+                } else {
+                    self.registers[FLAG_REGISTER] = 0;
+                }
+            },
+            //
+            // FLAG_REGISTER = 1 when:
+            // 1. The first operand is larger than or equal to the second operand
+            // FLAG_REGISETER = 0 when: (underflow)
+            // 1. The second operand is larger than the first operand
+            //
+            (0x8, _, _, 5) => {
+                let regx = self.registers[nibble2 as usize];
+                let regy = self.registers[nibble3 as usize];
+                self.registers[nibble2 as usize] = regx - regy;
+
+                if regx >= regy {
+                    self.registers[FLAG_REGISTER] = 1; 
+                } else {
+                    self.registers[FLAG_REGISTER] = 0; 
+                }
+            },
+            (0x8, _, _, 7) => {
+                let regx = self.registers[nibble2 as usize];
+                let regy = self.registers[nibble3 as usize];
+                self.registers[nibble2 as usize] = regy - regx;
+
+                if regy >= regx {
+                    self.registers[FLAG_REGISTER] = 1; 
+                } else {
+                    self.registers[FLAG_REGISTER] = 0; 
+                }
+            },
+            (_, _, _, _) => {
+                println!("Invalid opcode");
+            }
+        //    0x0 =>  {
+        //     self.display = [false; SCREEN_HEIGHT * SCREEN_WIDTH]
+        //    },
+        //    0x1 => {
+        //     let addr = instr & 0xFFF;
+        //     self.pc = addr;
+        //     debug_println!("pc = {:04X?}", self.pc);
+        //     debug_println!("addr = {:04X?}", addr);
+        //    },
+        //    0x6 => {
+        //     let reg = ((instr & 0xF00) >> BYTE) as usize;
+        //     let val = (instr & 0xFF) as u8;
+        //     self.registers[reg] = val;
+        //     debug_println!("Register[{reg}]: {:02X?}", self.registers[reg]);
+        //    },
+        //    0x7 => {
+        //     let reg = ((instr & 0xF00) >> BYTE) as usize;
+        //     let val = (instr & 0xFF) as u8;
+        //     self.registers[reg] += val;
+        //    }
+        //    0xA => {
+        //     let addr = instr & 0xFFF;
+        //     self.i_reg = addr;
+        //     debug_println!("i_reg: {:04X?}", self.i_reg);
+        //    }
+        //    0xD => {
+        //     let reg_x = ((instr & 0xF00) >> BYTE) as usize;
+        //     let reg_y = ((instr & 0x0F0) >> 4) as usize;
+        //     let x = self.registers[reg_x] as usize % SCREEN_WIDTH;
+        //     let y = self.registers[reg_y] as usize % SCREEN_HEIGHT;
+        //     let height = (instr & 0xF) as usize;
+            
+        //     self.registers[FLAG_REGISTER] = 0;
+
+        //     debug_println!("register[{reg_x}]: {x}");
+        //     debug_println!("register[{reg_y}]: {y}");
+        //     debug_println!("height(hex,dec): ({:02X?}, {height})", {height});
+        //     //set display
+        //     for i in 0..height {
+        //         let row = y + i;
+        //         if row <= SCREEN_HEIGHT {
+        //             let sprite_data = self.memory[(self.i_reg as usize) + i];
+        //             //for each bit in sprite_data
+        //             for bit in 0..BYTE {
+        //                 let col = x + bit;
+        //                 if col <= SCREEN_WIDTH {
+        //                     let sprite_bit = (sprite_data >> ((BYTE - 1) - bit)) & 1;
+        //                     let flatten_coord = (row * SCREEN_WIDTH) + col;
+        //                     let screen_pixel = &mut self.display[flatten_coord];
+
+        //                     if sprite_bit == 1 && *screen_pixel == true {
+        //                         self.registers[FLAG_REGISTER] = 1;
+        //                         *screen_pixel = false;
+        //                     } else if sprite_bit == 1 && *screen_pixel == false {
+        //                         *screen_pixel = true;
+        //                     }
+        //                 }   
+        //             }
+        //         }
+        //     }
+
+        //    }
+        //    _ => {
+        //     println!("Not implemented yet!");
+        //    }
         }
 
     }
@@ -173,7 +308,7 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let mut chip8 = Chip8 {
         memory: [0; RAM_SIZE],
         pc: 0x200,
-        //stack: [0; STACK_SIZE],
+        stack: Vec::with_capacity(STACK_SIZE),
         registers: [0; NUM_REG],
         i_reg: 0,
         //delay_timer: 0,
